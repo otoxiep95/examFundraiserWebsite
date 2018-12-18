3a5645ec3051530a4a473839c40f672fbb85f429
Mathilde Frachon <frachon.math@gmail.com>
2018-12-18 18:32:35 +0100

    commit after 2steps form and form forest done

--- myForest.js
+++ myForest.js
@@ -34,7 +34,7 @@ document.querySelector(".minus").addEventListener("click", e => {
   }
 });
 goBackButton.addEventListener("click", e => {
-  document.querySelector(".cc-part").classList.add("hidden");
+  document.querySelector(".myforestCC").classList.add("hidden");
   document.querySelector(".planttree-part").classList.remove("hidden");
 });
 
@@ -59,7 +59,7 @@ donationForm.addEventListener("submit", e => {
 });
 
 function goToPayment() {
-  document.querySelector(".cc-part").classList.remove("hidden");
+  document.querySelector(".myforestCC").classList.remove("hidden");
   document.querySelector(".planttree-part").classList.add("hidden");
 }
 
@@ -77,5 +77,5 @@ function postDonation(newDonation) {
     }
   })
     .then(res => res.json())
-    .then(d => {});
+    .then(d => { });
 }
